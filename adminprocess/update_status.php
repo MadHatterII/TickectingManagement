2cca20586b77ed7a7b25ae7aa4ce4ef5feccb348
Mikhael Gadiz <gadizkhael@gmail.com>
2023-11-23 22:07:59 +0800

    [khael] new and updated file for boat UI and boat process

--- adminprocess/update_status.php
+++ adminprocess/update_status.php
@@ -1,2 +1,24 @@
 
+<?php
+include '../connection/connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $boatId = $_POST['boatId'];
+    $newStatus = $_POST['newStatus'];
+
+    // Update the status in the database
+    $updateQuery = "UPDATE boats SET boat_status = '$newStatus' WHERE id = $boatId";
+    $updateResult = $conn->query($updateQuery);
+
+    if ($updateResult) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false, 'message' => 'Error updating status']);
+    }
+} else {
+    echo json_encode(['success' => false, 'message' => 'Invalid request method']);
+}
+
+$conn->close();
+?>
 
