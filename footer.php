0682d9f945fafb3565f9db3ba9389e0b56b2376e
Berniegwapo <berniecalope30@gmail.com>
2023-11-24 13:19:40 -0800

    [Calope] try t2

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
                 <strong>@ Copyright &copy; Canigao Island Ticketing System: Advance Database.</strong>
                 Made by: Goup 3.
                 <div class="float-right d-none d-sm-inline-block">
-                    <b>AY</b>  :2023-2024
+                    <b>AY</b>  :2023-2028
                 </div>
             </footer>
