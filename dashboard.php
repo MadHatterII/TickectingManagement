ea561d14d3d352ec8ba5a8167a4052ed7e2e36b6
BSITkuizon <pitockyle3@gmail.com>
2023-11-26 20:39:47 +0800

    [Kuizon] Kani gamita nga dashboard

--- dashboard.php
+++ dashboard.php
@@ -46,7 +46,118 @@
 
     <?php include '../adminsidebar/navbar.php'; ?>
 
-    <?php include '../adminsidebar/boatside.php'; ?>
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="adminhome.php" class="brand-link">
+                <img src="../img/slsulogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: 1">
+                <span class="brand-text font-weight-light">WanderLust</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user panel (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="../img/canigs.png" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">Alexander Pierce</a>
+                    </div>
+                </div>
+
+
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                        <li class="nav-item menu-open">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>
+                                    Dashboard
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="userdash.php" class="nav-link ">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Dashboard </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="userticket.php" class="nav-link active">
+                                        <i class="far fa-user nav-icon"></i>
+                                        <p> Agent Management</p>
+                                    </a>
+                                </li>
+                                <!-- <li class="nav-item">
+                                    <a href="adminprice.php" class="nav-link">
+                                        <i class="fas fa-money-bill nav-icon"></i>
+                                        <p>Price Management</p>
+                                    </a>
+                                </li> -->
+
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-check-circle nav-icon"></i>
+                                <p>
+                                    Cottage/Boat Status
+                                    <i class="fas fa-angle-left right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="userboat.php" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Boat</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="usercottage.php" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Cottage</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-header">MISCELLANEOUS</li>
+
+
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-chart-line text-info"></i>
+                                <p>Report</p>
+                            </a>
+                        </li>
+
+
+
+                        <li class="nav-item">
+                            <a href="/logout" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Logout</p>
+                            </a>
+                        </li>
+
+
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
+
+
+
+        <!-- Sidebar -->
 
     <!-- Sidebar -->
 
@@ -109,5 +220,5 @@
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
   <script src="../dist/js/pages/dashboard.js"></script>
 </body>
-
-</html>
+  
+</html> 
