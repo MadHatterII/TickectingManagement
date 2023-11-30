fc731be5ef29278df6dfc551d49f5aff2056d7ba
erica-erica <151019794+erica-erica@users.noreply.github.com>
2023-11-30 11:07:04 +0800

    [Suarez and Fuents]added customer table in admin

--- admin/customerside.php
+++ admin/customerside.php
@@ -1,18 +1,4 @@
-<?php
-// Assuming you have a database connection established
-include '../connection/connection.php';
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-// Fetch data from the database or any other data source
-$query = "SELECT * FROM members";
-$result = $conn->query($query);
-
-// Close the database connection
-$conn->close();
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -68,63 +54,154 @@ $conn->close();
 
 
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <div class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1 class="m-0">Customer Information</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                
-              </ol>
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-      </div>
-      <!-- /.content-header -->
+    
+ 
+      
+        <!-- Add this button in the table loop to trigger the modal -->
+<td>
+    <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#customerModal<?php echo $row['id']; ?>">
+        View Details
+    </button>
+    <?php
+// Assuming you have a database connection established
+include '../connection/connection.php';
 
-      <!-- Main content -->
-      <section class="content">
-      <!DOCTYPE html>
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch data from the database or any other data source
+$query = "SELECT * FROM members";
+$result = $conn->query($query);
+?>
+
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin | Customer Information</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="../plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.min.css">
     <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 20px;
+        .sidebar {
+            background: rgb(13, 126, 194);
+            background: linear-gradient(0deg, rgba(13, 126, 194, .453321321) 58%, rgba(208, 170, 89, 0.8548669467787114) 77%);
         }
-        th, td {
-            border: 1px solid #ddd;
-            padding: 10px;
-            text-align: left;
-        }
+
         th {
-            background-color: #00ff00;
+            background-color: #3ea175; /* Header background color */
+            color: #fff;
         }
     </style>
 </head>
-<body>
-<table>
-    <thead>
-        <tr>
-            <th>groupName</th>
-            <th>name</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php
-        // Loop through the fetched data and populate the table
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Customer Information</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Group Name</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Loop through the fetched data and populate the table
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td>" . $row["groupName"] . "</td>";
+                            echo "<td><button type='button' class='btn btn-primary btn-sm' data-toggle='modal' data-target='#customerModal" . $row['id'] . "'>View Details</button></td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Modal -->
+        <?php
+        // Reset the result set pointer
+        $result->data_seek(0);
+
+        // Modal structure outside the loop
         while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row["groupName"] . "</td>";
-            echo "<td>" . $row["name"] . "</td>";
-            echo "</tr>";
+            echo "<div class='modal fade' id='customerModal" . $row['id'] . "' tabindex='-1' role='dialog' aria-labelledby='customerModalLabel' aria-hidden='true'>";
+            echo "<div class='modal-dialog' role='document'>";
+            echo "<div class='modal-content'>";
+            echo "<div class='modal-header'>";
+            echo "<h5 class='modal-title' id='customerModalLabel'>Customer Details</h5>";
+            echo "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>";
+            echo "<span aria-hidden='true'>&times;</span>";
+            echo "</button>";
+            echo "</div>";
+            echo "<div class='modal-body'>";
+            echo "<p><strong>Group Name:</strong> " . $row['groupName'] . "</p>";
+            // Add more details as needed
+            echo "</div>";
+            echo "<div class='modal-footer'>";
+            echo "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
         }
         ?>
+
+        <?php include '../footer.php' ?>
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- Your script imports go here -->
+
+    </body>
+
+</html>
+
+</td>
     </tbody>
 </table>
 
@@ -135,7 +212,7 @@ $conn->close();
     <!-- /.content -->
 
     <!-- /.content-wrapper -->
-    <?php include '../footer.php' ?>
+   
 
 
   </div>
